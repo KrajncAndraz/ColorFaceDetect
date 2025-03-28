8833dd0027aea988aa5e7a8119da81c6f9227554
KrajncAndraz <andraz.krajnc1@student.um.si>
2025-03-28 14:07:08 +0100

    Implement video capture and face outline

--- colorFaceDetect.py
+++ colorFaceDetect.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import time as t
 
 def zmanjsaj_sliko(slika, sirina, visina):
     slika = cv.resize(slika, (sirina, visina), interpolation=cv.INTER_AREA)
@@ -73,18 +74,72 @@ def doloci_barvo_koze(slika,levo_zgoraj,desno_spodaj) -> tuple:
 
 if __name__ == '__main__':
     #Pripravi kamero
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
 
     #Zajami prvo sliko iz kamere
+    ret, firstFrame = cap.read()
+    if not ret:
+        cap.release()
+        cv.destroyAllWindows()
+        print("Can't receive frame (stream end?). Exiting ...")
+        exit()
+
+    firstFrame = zmanjsaj_sliko(firstFrame, 260, 300)
 
     #Izračunamo barvo kože na prvi sliki
+    barva = doloci_barvo_koze(firstFrame, (105,125), (155,175))
+
+    prev_time = 0
 
     #Zajemaj slike iz kamere in jih obdeluj     
-    
-    #Označi območja (škatle), kjer se nahaja obraz (kako je prepuščeno vaši domišljiji)
+    while True:
+        ret, frame = cap.read()
+ 
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        if cv.waitKey(1) == ord('q'):
+            break
+
+        frame = zmanjsaj_sliko(frame, 260, 300)
+
+        #Označi območja (škatle), kjer se nahaja obraz (kako je prepuščeno vaši domišljiji)
         #Vprašanje 1: Kako iz števila pikslov iz vsake škatle določiti celotno območje obraza (Floodfill)?
         #Vprašanje 2: Kako prešteti število ljudi?
+        boxSize = 10;
+        obraz = obdelaj_sliko_s_skatlami(frame, boxSize, boxSize, barva)
+
+        min_x, min_y = float('inf'), float('inf')
+        max_x, max_y = float('-inf'), float('-inf')
+
+        for y, row in enumerate(obraz):
+            for x, val in enumerate(row):
+                if val == 1:
+                    min_x = min(min_x, x)
+                    min_y = min(min_y, y)
+                    max_x = max(max_x, x)
+                    max_y = max(max_y, y)
+
+        if min_x < float('inf') and min_y < float('inf'):
+            top_left = (min_x * boxSize, min_y * boxSize)
+            bottom_right = ((max_x + 1) * boxSize, (max_y + 1) * boxSize)
+            cv.rectangle(frame, top_left, bottom_right, (0, 255, 0), 2)
+
+
+
+        current_time = t.time()
+        fps = 1 / (current_time - prev_time)
+        prev_time = current_time
+        cv.putText(frame, f"FPS: {int(fps)}", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        cv.imshow('frame', frame)
 
         #Kako velikost prebirne škatle vpliva na hitrost algoritma in točnost detekcije? Poigrajte se s parametroma velikost_skatle
         #in ne pozabite, da ni nujno da je škatla kvadratna.
-    pass
 
+    cap.release()
+    cv.destroyAllWindows()
